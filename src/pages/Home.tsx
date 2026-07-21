2c0faf7f981af9f2e2b272cb5fcf9823a2f31f2b
Rahul R P <rahul.rp@triassicsolutions.com>
2026-07-21 10:54:17 +0530

    remove active traders text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <ProfitSplit />
       <Features />
       <WhyChooseUs />
-      <LiveStats />
+      {/* <LiveStats /> */}
       <Testimonials />
       <CTA />
       <Footer />
